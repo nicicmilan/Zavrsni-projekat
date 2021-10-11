8bf1f3cdaa32f53b8e105635c28ec4bcfda14254
Milan Nicic <nicicmilan1@gmail.com>
2021-10-11 19:32:35 +0200

    Added NotificationSystemPage

---
+++ src/pages/NotificationSystemPage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NotificationSystemPage extends BasicPage {
+
+	public NotificationSystemPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		super(driver, wait, js);
+	}
+
+	public WebElement getElementMsg() {
+		return driver.findElement(By.xpath("//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
+	}
+	public String getMsg() {
+		return this.getElementMsg().getText();
+	}
+	
+	public void notificationInvisible() {
+		WebElement systemMessage = driver.findElement(By.xpath("//*[contains(@class, 'system_message')]"));
+		wait.until(ExpectedConditions.attributeToBe(systemMessage, "style", "display: none;"));
+	}
+}
